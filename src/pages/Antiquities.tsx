897b3a530afec9d8c9043a35722c1c18448cebca
Agnieszka Konefał <agnieszka.konefal@allegro.pl>
2024-01-22 20:35:44 +0100

    TC-1 | Add mantine and json data for antiquities.

---
+++ src/pages/Antiquities.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Antiquities() {
+  return (
+    <div>
+      <h2>Antiquities in Alik'r Desert</h2>
+      <p> Here will be the list of antiquities.</p>
+    </div>
+  );
+}
