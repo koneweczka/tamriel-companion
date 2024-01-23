9be6ad4a92c2a506a90dfcdaed25f410fd40c707
Agnieszka Konefał <agnieszka.konefal@allegro.pl>
2024-01-23 12:45:16 +0100

    TC-1 | Add fauna and client.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Client } from 'fauna';
 import { MantineProvider } from '@mantine/core';
 import App from './App.tsx';
 import './index.css';
+
+const client = new Client({
+  // TODO: Move it later to env variables:
+  secret: 'mySercret',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
