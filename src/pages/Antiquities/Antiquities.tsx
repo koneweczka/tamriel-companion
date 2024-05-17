c302883ff6527e280fd33b8ca0961b291935ca69
Agnieszka Konefał <agnieszka.konefal@allegro.pl>
2024-05-17 13:42:28 +0200

    TC-1 | Add accordians with locations.

--- src/pages/Antiquities/Antiquities.tsx
+++ src/pages/Antiquities/Antiquities.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button, Container, Flex, Title } from '@mantine/core';
+import { Accordion, Avatar, Container, Group, Text, Title } from '@mantine/core';
+import { locations } from '../../utils/recordMaps';
 import classes from './Antiquities.css';
 
 export function Antiquities() {
@@ -8,15 +9,37 @@ export function Antiquities() {
       <Title order={1} className={classes.title}>
         Antiquities
       </Title>
-      <Flex direction="column" gap={{ base: 'sm', sm: 'lg' }} className={classes.buttonFlex}>
-        <Button color="cyan.9">Aldmeri Dominion</Button>
-        <Button color="cyan.9">Daggerfall Covenant</Button>
-        <Button color="cyan.9">Ebonheart Pact</Button>
-        <Button color="cyan.9">Coldharbour</Button>
-        <Button color="cyan.9">Craglorn</Button>
-        <Button color="cyan.9">Eyevea</Button>
-        <Button color="cyan.9">DLC Areas</Button>
-      </Flex>
+      <Accordion>
+        {locations.map((location) => (
+          <Accordion.Item key={location.name} value={location.name}>
+            <Accordion.Control>
+              <AccordionLabel label={location.name} avatar={location.avatar} />
+            </Accordion.Control>
+            <Accordion.Panel>
+              {location.areas.map((area) => (
+                <Accordion.Item key={area.name} value={area.name}>
+                  <Accordion.Control>
+                    <AccordionLabel label={area.name} avatar={area.avatar} />
+                  </Accordion.Control>
+                  <Accordion.Panel>
+                    <Text>Content</Text>
+                  </Accordion.Panel>
+                </Accordion.Item>
+              ))}
+            </Accordion.Panel>
+          </Accordion.Item>
+        ))}
+      </Accordion>
     </Container>
+  );
+}
+function AccordionLabel({ label, avatar }: { label: string; avatar?: string }) {
+  return (
+    <Group wrap="nowrap">
+      {avatar && <Avatar src={avatar} radius="xl" size="lg" />}
+      <div>
+        <Text>{label}</Text>
+      </div>
+    </Group>
   );
 }
